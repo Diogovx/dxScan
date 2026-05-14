15af4d5038d684ea48f96e5e427c3875e3b89ce3
Diogo Velozo Xavier <diogo.velozo@outlook.com>
2026-05-14 11:44:19 -0300

    :sparkles: feat: Added support to 100 requests simultaneously

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -4,7 +4,12 @@ from app.core.config import settings
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=False)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_size=50,
+    max_overflow=100,
+    pool_timeout=30
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
